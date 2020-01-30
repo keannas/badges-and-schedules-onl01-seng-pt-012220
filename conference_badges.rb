3bdc3994c43f9c625f57bebf46f2481a1d71acb7
Keanna Salter <keannasalter91@gmail.com>
2020-01-30 09:20:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker ("Arel")
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  
