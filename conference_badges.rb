e59b62258e97f732fdfe52262d6d97d9f82c11c2
Keanna Salter <keannasalter91@gmail.com>
2020-01-30 09:30:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,4 +6,24 @@ end
 
 
 def batch_badge_creator(array)
-  
+  nuarray = []
+  array.each do |name|
+    nurray.push ("Hello my name is #{name}.")
+  end
+  return nurray
+end
+
+# batch_badge_creator (people)
+
+
+
+
+def assign_rooms (array)
+  nurray = []
+  counter = l
+  array.each do |name|
+    nuarray.push ("Hello, #{name}! You'll be assigned to room #{counter} !")
+    counter += 1
+end
+
+# assign_rooms (people)
